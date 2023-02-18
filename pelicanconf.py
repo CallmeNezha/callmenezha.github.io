f122035985f626d9324fee676dba50a3b2806672
NeZha <783627014@qq.com>
2023-02-18 21:15:22 +0800

    First commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,12 +2,43 @@ AUTHOR = 'Zijian Jiang'
 SITENAME = "Zijian Jiang's Journal"
 SITEURL = ''
 
-PATH = 'content'
-OUTPUT_PATH = 'docs'
-
 TIMEZONE = 'Asia/Shanghai'
-
 DEFAULT_LANG = 'en'
+
+SUBTITLE = 'Papyrus'
+SUBTEXT = '''A fast and responsive theme built for the <a class="underline" 
+href="https://blog.getpelican.com/">Pelican</a> site generator.<br>
+The theme is inspired from <a class="underline" 
+href="https://github.com/adityatelange/hugo-PaperMod">Hugo-PaperMod</a>. 
+It is styled using <a class="underline" 
+href="https://tailwindcss.com/">Tailwind CSS</a>. 
+It supports dark mode and built in search function.
+'''
+COPYRIGHT = '©2022'
+PATH = 'content'
+THEME = 'themes/Papyrus'
+OUTPUT_PATH = 'docs'
+THEME_STATIC_PATHS = ['static']
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['readtime', 'neighbors', 'pelican-toc']
+
+DISPLAY_PAGES_ON_MENU = True
+DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'about'))
+PAGINATED_TEMPLATES = {'index':None,'tag':None,'category':None,'author':None,'archives':24,}
+
+# Site search plugin
+# SEARCH_MODE = "docs"
+# SEARCH_HTML_SELECTOR = "main"
+# Table of Content Plugin
+TOC = {
+    'TOC_HEADERS'       : '^h[1-3]', # What headers should be included in
+                                     # the generated toc
+                                     # Expected format is a regular expression
+    'TOC_RUN'           : 'true',    # Default value for toc generation,
+                                     # if it does not evaluate
+                                     # to 'true' no toc will be generated
+    'TOC_INCLUDE_TITLE': 'false',    # If 'true' include title in toc
+}
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -15,18 +46,28 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+RSS_FEED_SUMMARY_ONLY = None
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# Social widgets
+SOCIAL = (
+    ('github', 'https://github.com/aleylara/Papyrus'),
+    ('twitter', 'https://twitter.com/'),
+)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# Article share widgets
+SHARE = (
+    ("twitter", "https://twitter.com/intent/tweet/?text=Features&amp;url="),
+    ("linkedin", "https://www.linkedin.com/sharing/share-offsite/?url="),
+    ("reddit", "https://reddit.com/submit?url="),
+    ("facebook", "https://facebook.com/sharer/sharer.php?u="),
+    ("whatsapp", "https://api.whatsapp.com/send?text=Features - "),
+    ("telegram", "https://telegram.me/share/url?text=Features&amp;url="),
+)
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 8
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# DISQUS_SITENAME = ''
+# GOOGLE_ANALYTICS = ''
